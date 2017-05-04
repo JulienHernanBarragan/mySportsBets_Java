4e3964668bb283ee116d2627b077b67e2e1be006
Hernandez Julien <julienhernandez14@gmail.com>
2017-05-04 09:24:50 +0200

    add class Connect_DB and Main

---
+++ mySportsBets_TP/src/databaseProcessing/Connect_DB.java
@@ -0,0 +1,25 @@
+package databaseProcessing;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import com.mysql.jdbc.Connection;
+
+public class Connect_DB {
+
+	Connection connexion;
+		String url;
+		
+		public Connect_DB()	{
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+				url = new String("jdbc:mysql://localhost:3306/mysportbets");
+				connexion = (Connection) DriverManager.getConnection(url,"root","");
+				System.out.println("Connexion réussi");
+			} catch (ClassNotFoundException e) {
+				System.out.println(e.getMessage());
+			} catch (SQLException e) {
+				System.out.println(e.getMessage());
+			}
+		}
+}
---
+++ mySportsBets_TP/src/exe/Main.java
@@ -0,0 +1,11 @@
+package exe;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		new databaseProcessing.Connect_DB();
+
+	}
+
+}
