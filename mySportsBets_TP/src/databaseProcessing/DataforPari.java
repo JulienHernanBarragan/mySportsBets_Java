059d662fa9b95672c6e066719707066c1de6c45d
Hernandez Julien <julienhernandez14@gmail.com>
2017-05-04 10:16:35 +0200

    add firstView for Bankroll (just text), possibility bankroll per Month.

--- mySportsBets_TP/src/databaseProcessing/DataforPari.java
+++ mySportsBets_TP/src/databaseProcessing/DataforPari.java
@@ -9,18 +9,21 @@ public class DataforPari {
 
 	//Fonction pour connexion à la base de données pour bankroll
     public String dataBankroll(String datepari) {	
-    	int BDnmbpari = 0, BDmisestotale = 0, BDgainstotal = 0;
+    	int BDnmbpari = 0, BDmisestotale = 0, BDgainstotal = 0, BDnmbMatchTotal = 0, BDnmbMGTotal = 0;
     	
     	try 
 	    {
 	    	Statement st = new Connect_DB().connexion.createStatement();
-	    	ResultSet requestPari = st.executeQuery("SELECT COUNT(ID) AS nmbpari, SUM(mises) AS misestotale, SUM(gains) AS gainstotal "+
+	    	ResultSet requestPari = st.executeQuery("SELECT COUNT(ID) AS nmbpari, SUM(mises) AS misestotale, SUM(gains) AS gainstotal, "+
+	    											"SUM(nmbMatch) AS nmbMatchTotal, SUM(nmbmatchGagnant) AS nmbMGTotal "+
 	    											"FROM pari "+
 	    											"WHERE datePari between "+datepari);
 	    		while (requestPari.next()) {
 	    			BDnmbpari = requestPari.getInt("nmbpari");
 	    			BDmisestotale = requestPari.getInt("misestotale");
 	    			BDgainstotal = requestPari.getInt("gainstotal");
+	    			BDnmbMatchTotal = requestPari.getInt("nmbMatchTotal");
+	    			BDnmbMGTotal = requestPari.getInt("nmbMGTotal");
 	    		}
 	    		requestPari.close();
 
@@ -28,9 +31,9 @@ public class DataforPari {
 	    catch (Exception e)
 	    {
 	    	System.out.println("Connexion impossible");
-	    e.printStackTrace();
+	    	e.printStackTrace();
 	    }
-	    return Integer.toString(BDnmbpari) +","+ Integer.toString(BDmisestotale) +","+ Integer.toString(BDgainstotal);
+	    return Integer.toString(BDnmbpari) +","+ Integer.toString(BDmisestotale) +","+ Integer.toString(BDgainstotal) +","+ Integer.toString(BDnmbMatchTotal) +","+ Integer.toString(BDnmbMGTotal);
     }
 
 }
--- mySportsBets_TP/src/exe/Main.java
+++ mySportsBets_TP/src/exe/Main.java
@@ -1,11 +1,15 @@
 package exe;
 
+import api.Bankroll;
+
 public class Main {
 
 	public static void main(String[] args) {
 
 		String test = new databaseProcessing.DataforPari().dataBankroll("'2017/01/01' and '2017/12/31'");
 		System.out.println(test);
+		Bankroll session = new Bankroll();
+		session.setVisible(true);
 	}
 
 }
